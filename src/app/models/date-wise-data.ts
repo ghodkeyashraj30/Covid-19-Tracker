37652ca97e6d03806a7e7e165f1bb08a970d094b
Yashraj Ghodke <78252070+ghodkeyashraj30@users.noreply.github.com>
2021-08-31 19:01:15 +0530

    Add files via upload

---
+++ src/app/models/date-wise-data.ts
@@ -0,0 +1,5 @@
+export interface DateWiseData{
+    country ?:string | any , 
+    cases ?: number | any , 
+    date  ?: Date | any
+}
---
+++ src/app/models/global-data.ts
@@ -0,0 +1,7 @@
+export interface GlobalDataSummary{
+    country ?:string | any,
+    confirmed ?:number | any,
+    deaths ?:number | any,
+    recovered ?:number | any,
+    active ?:number | any 
+}
